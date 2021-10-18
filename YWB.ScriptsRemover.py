0cf0db45cb6ca2878f0ba807ba9863541ac745cd
Daniel Vygolov <rattack@gmail.com>
2021-10-18 23:25:56 +0400

    Code refactoring Added stripping HTML comments Modified JQuery detection

--- YWB.ScriptsRemover.py
+++ YWB.ScriptsRemover.py
@@ -1,7 +1,11 @@
-import os,time,re
-from bs4 import BeautifulSoup, Doctype
+import os
+import time
+import re
+from bs4 import BeautifulSoup, Comment
+from urllib.parse import urlparse
 
-jquery="//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" #Change JQuery version if you need
+jquery = "//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" #Change JQuery version if you need
+
 php_sig = '!!!PHP!!!'
 php_elements = []
 def php_remove(m):
@@ -19,25 +23,56 @@ def remove_all_scripts(soup):
         print('Found noscript tag, removing...')
         s.extract()
 
+def is_local_jquery(str):
+    if str == None:
+       return False
+    if bool(urlparse(str).netloc):
+        return False
+    if str.endswith('jquery.js') or str.endswith('jquery.min.js'):
+        return True
+    return False
+
+def is_google_tag(tag):
+    if tag.string== None:
+        return False
+    if "gtag('config" in tag.string:
+        return True
+    if tag.get('src') == None:
+        return False
+    if "googletagmanager" in tag.get('src'):
+        return True
+
+def is_facebook_tag(tag):
+    if tag.string==None:
+        return False
+    if "fbq('init'" in tag.string or "fbq('track" in tag.string:
+        return True
+
+def is_yandex_tag(tag):
+    if tag.string==None:
+        return False
+    if "mc.yandex.ru" in tag.string:
+        return True
+
 def modify_scripts(soup):
     for s in soup.select('noscript'):
         print('Found noscript tag, removing...')
         s.extract()
     for s in soup.select('script'):
-        if (s.string!=None and ("fbq('init'" in s.string or "fbq('track" in s.string)):
+        if is_facebook_tag(s):
             print('Found FB pixel\'s tag, removing...')
             s.extract()
-        elif s.string!=None and "mc.yandex.ru" in s.string:
+        elif is_yandex_tag(s):
             print('Found Yandex Metrika\'s tag, removing...')
             s.extract()
-        elif (s.string!=None and "gtag('config" in s.string) or (s.get('src')!=None and "googletagmanager" in s.get('src')):
+        elif is_google_tag(s):
             print('Found Google tag, removing...')
             s.extract()
-        elif s.get('src')!=None and "jquery" in s.get('src'):
-            print('Found JQuery tag, modifying...')
-            s['src']=jquery
+        elif is_local_jquery(s.get('src')):
+            print(f'Found local JQuery {s.get("src")}, modifying...')
+            s['src'] = jquery
 
-print('HTML Scripts Remover ver. 0.3a by Yellow Web')
+print('HTML Scripts Remover ver. 0.4a by Yellow Web')
 print('If you like this script, PLEASE DONATE!')
 print("WebMoney: Z182653170916")
 print("Bitcoin: bc1qqv99jasckntqnk0pkjnrjtpwu0yurm0qd0gnqv")
@@ -53,7 +88,7 @@ for root, dirnames, filenames in os.walk(dir_path):
             fname = os.path.join(root, filename)
             print('Found file: {}'.format(fname))
             files.append(fname)
-if files.count==0:
+if files.count == 0:
     print('No HTML/PHP files found! Start script in a directory WITH HTML/PHP files!')
     exit
 
@@ -66,10 +101,10 @@ menu = input()
 
 for fname in files:
     print(f'Processing {fname}...')
-    f=open(fname,'r')
-    html=f.read()
+    f = open(fname,'r')
+    html = f.read()
     # Pre-parse HTML to remove all PHP elements
-    html = re.sub(r'<\?.*?\?>', php_remove, html, flags=re.S+re.M)
+    html = re.sub(r'<\?.*?\?>', php_remove, html, flags=re.S + re.M)
     soup = BeautifulSoup(html, 'html.parser')
     try:
         match menu:
@@ -77,6 +112,10 @@ for fname in files:
                 remove_all_scripts(soup)
             case '2':
                 modify_scripts(soup)
+        print('Removing all HTML comments...')
+        comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+        for comment in comments:
+            comment.extract()
         html = re.sub(php_sig, php_add, soup.prettify(formatter="html"))
     except:
         raise
